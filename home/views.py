be537fa804307260317adfee8f690366c2daad9d
unuvararzu653@gmail.com <62670222+arzunuvar@users.noreply.github.com>
2020-04-05 22:00:05 +0300

    contact formu oluşturuldu
    contact bize ulaşın formu oluşturuldu ve tablo ikiye ayrıldı.

--- home/views.py
+++ home/views.py
@@ -1,8 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-
+from django.contrib import messages
 # Create your views here.
-from home.models import Setting
+from home.models import Setting, ContactFormu, ContactFormMessage
 
 
 def index(request):
@@ -24,6 +24,21 @@ def referanslar(request):
 
 
 def iletisim(request):
+
+    if request.method == 'POST': # form post edildiyse
+        form = ContactFormu(request.POST)
+        if form.is_valid():
+            data = ContactFormMessage() # model ile bağlantı kur
+            data.name = form.cleaned_data['name'] # formdan bilgiyi al
+            data.email = form.cleaned_data['email']
+            data.subject = form.cleaned_data['subject']
+            data.message = form.cleaned_data['message']
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save() # veritabanına kaydet
+            messages.success(request, "Mesajınız başarı ile gönderilmiştir.Teşekkür Ederiz ")
+            return HttpResponseRedirect ('/iletisim')
+
     setting = Setting.objects.get(pk=1)
-    context = {'setting': setting, 'page': 'iletisim'}
+    form = ContactFormu()
+    context = {'setting': setting, 'form': form}
     return render(request, 'iletisim.html', context)
